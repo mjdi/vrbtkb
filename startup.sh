192d9015c09d211dfabdbf2556d34c7c1f1368f0
mjdi <mjdiemand@gmail.com>
2016-12-21 09:06:57 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,12 @@
 #!bin/usr/env bash
 
 #xterm -hold -e 'sudo /etc/init.d/bluetooth stop; sudo /usr/sbin/bluetoothd --nodetach --debug -p time' &
-sudo /etc/init.d/bluetooth stop; sudo /usr/sbin/bluetoothd --nodetach --debug -p time &
 
 sleep 5
 
 #xterm -hold -e 'sudo hciconfig hcio; sudo hciconfig hcio up; sudo hciconfig hcio lm master' &
-sudo hciconfig hcio; sudo hciconfig hcio up; sudo hciconfig hcio lm master &
 
 sleep 5
 
 #xterm -hold -e 'sudo python /home/pi/vrbtkb/btkeyboard/bt_kb_client.py' &
-sudo python /home/mjd/vrbtkb/vrkeyboard/vr_kb_client.py &
+xterm -hold -e 'sudo python /home/chip/vrbtkb/vrkeyboard/vr_kb_client.py' &
