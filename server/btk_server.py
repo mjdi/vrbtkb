c705e34c3fd613d617cd9daaa49f55724c8e1a6e
mjdi <mjdiemand@gmail.com>
2017-01-11 06:35:10 -0500

    Update btk_server.py

--- server/btk_server.py
+++ server/btk_server.py
@@ -177,8 +177,7 @@ class BTKbDevice():
         print ("Got a connection on the interrupt channel from " + cinfo[0])
 
 	# ==== Iniiate vr_kb_client.py server as soon as connection is made, (not before), but only when headless is working! ==== #
-	# os.system("sudo python ./vrbtkb/vrkeyboard/vr_kb_client.py &")
-	os.system("xterm -hold -e 'sudo python ./vrbtkb/vrkeyboard/vr_kb_client.py' &")
+	os.system("sudo python ./vrbtkb/vrkeyboard/vr_kb_client.py &")
 
     #send a string to the bluetooth host machine
     def send_string(self,message):
