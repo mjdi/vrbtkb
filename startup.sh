bae8003ee52cfb33237109b595ccf47cd62fc71f
mjdi <mjdiemand@gmail.com>
2017-02-08 18:12:14 -0500

    Update and rename ssh_startup.sh to startup.sh

--- startup.sh
+++ startup.sh
@@ -8,4 +8,5 @@ sudo hciconfig hcio; sudo hciconfig hcio up; sudo hciconfig hcio lm master &
 
 sleep 5
 
-sudo python /home/chip/vrbtkb/server/btk_server.py &
+sudo python ~/vrbtkb/server/btk_server.py left &
+# sudo python ~/vrbtkb/server/btk_server.py right &
