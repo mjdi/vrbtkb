66597ea1d0a5b4eb48a8c0e19a16c7d74de07949
mjdi <mjdiemand@gmail.com>
2017-03-08 15:50:52 -0500

    Update kb_client.py

--- vrkeyboard/kb_client.py
+++ vrkeyboard/kb_client.py
@@ -646,12 +646,11 @@ def debug_joystick_cycle(kb):
 def get_btns_state(kb):
 
 	kb.btns_state = [0,0,0,0,0]
-	
-	kb.btns_state[0] = int( not bool(ReadChannel(0)) ) 	# off = ~1024 , on = 0 (we transform this to 0 off and 1 on) 
-	kb.btns_state[1] = GPIO.input(kb.btn2_pin) 	# off = 0, on = 1
+	kb.btns_state[0] = GPIO.input(kb.btn5_pin)	# off = 0, on = 1
+	kb.btns_state[1] = GPIO.input(kb.btn4_pin)	# off = 0, on = 1
 	kb.btns_state[2] = GPIO.input(kb.btn3_pin)	# off = 0, on = 1
-	kb.btns_state[3] = GPIO.input(kb.btn4_pin)	# off = 0, on = 1
-    	kb.btns_state[4] = GPIO.input(kb.btn5_pin)	# off = 0, on = 1
+	kb.btns_state[3] = GPIO.input(kb.btn2_pin) 	# off = 0, on = 1
+    	kb.btns_state[4] = int( not bool(ReadChannel(0)) ) 	# off = ~1024 , on = 0 (we transform this to 0 off and 1 on)
 	
 	kb.num_btns_pressed = sum(kb.btns_state)
 
