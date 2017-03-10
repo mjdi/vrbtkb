983ce0c49965d0063568cf544cef3b4f7c94ddfb
mjdi <mjdiemand@gmail.com>
2017-03-10 06:04:49 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,5 +8,7 @@ sudo hciconfig hcio; sudo hciconfig hcio up; sudo hciconfig hcio lm master &
 
 sleep 3
 
+device=$(ls /home | perl -pe 'chomp if eof')
+
 sudo python /home/$device/vrbtkb/server/btk_server.py left & # using ~/ causes /root/ instead
 # sudo python /home/$device/vrbtkb/server/btk_server.py right &
